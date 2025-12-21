850b57254a28e7d7866e1ce9e6f723385185ce21
Productive <skittlesfrosty123@gmail.com>
2025-12-21 14:07:14 +0800

    Create teams table

--- database/migrations/2025_12_21_060622_create_teams_table.php
+++ database/migrations/2025_12_21_060622_create_teams_table.php
@@ -11,12 +11,11 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('teams', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->string('password');
-            $table->enum('role', ['admin', 'operator'])->default('operator');
+            $table->string('name')->unique();
+            $table->string('logo_url')->nullable();
+            $table->string('home_base')->nullable();
             $table->timestamps();
         });
     }
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('teams');
     }
 };
