f8d0849925a145449d2f6252c598ef5917e67854
Alllync <skittlesfrosty123@gmail.com>
2025-12-22 22:41:23 +0800

    Fix: Match Event and Match game rename, and move store match request to correct path.

--- app/Models/MatchEvent.php
+++ app/Models/MatchEvent.php
@@ -5,37 +5,24 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MatchGame extends Model
+class MatchEvent extends Model
 {
     use HasFactory;
 
-    // Kita definisikan nama tabelnya eksplisit biar aman
-    protected $table = 'matches';
-
     protected $fillable = [
-        'home_team_id',
-        'away_team_id',
-        'match_date',
-        'home_score',
-        'away_score',
-        'status', // scheduled, finished, live
+        'match_id',
+        'player_id',
+        'event_type', // goal, yellow_card, red_card, own_goal
+        'minute',
     ];
 
-    // Relasi ke Tim Tuan Rumah
-    public function homeTeam()
+    public function match()
     {
-        return $this->belongsTo(Team::class, 'home_team_id');
+        return $this->belongsTo(MatchGame::class, 'match_id');
     }
 
-    // Relasi ke Tim Tamu
-    public function awayTeam()
+    public function player()
     {
-        return $this->belongsTo(Team::class, 'away_team_id');
-    }
-
-    // Relasi ke Event (Gol, Kartu, dll)
-    public function events()
-    {
-        return $this->hasMany(MatchEvent::class, 'match_id');
+        return $this->belongsTo(Player::class);
     }
 }
--- app/Models/MatchGame.php
+++ app/Models/MatchGame.php
@@ -5,24 +5,37 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class MatchEvent extends Model
+class MatchGame extends Model
 {
     use HasFactory;
 
+    // Kita definisikan nama tabelnya eksplisit biar aman
+    protected $table = 'matches';
+
     protected $fillable = [
-        'match_id',
-        'player_id',
-        'event_type', // goal, yellow_card, red_card, own_goal
-        'minute',
+        'home_team_id',
+        'away_team_id',
+        'match_date',
+        'home_score',
+        'away_score',
+        'status', // scheduled, finished, live
     ];
 
-    public function match()
+    // Relasi ke Tim Tuan Rumah
+    public function homeTeam()
     {
-        return $this->belongsTo(MatchGame::class, 'match_id');
+        return $this->belongsTo(Team::class, 'home_team_id');
     }
 
-    public function player()
+    // Relasi ke Tim Tamu
+    public function awayTeam()
     {
-        return $this->belongsTo(Player::class);
+        return $this->belongsTo(Team::class, 'away_team_id');
+    }
+
+    // Relasi ke Event (Gol, Kartu, dll)
+    public function events()
+    {
+        return $this->hasMany(MatchEvent::class, 'match_id');
     }
 }
